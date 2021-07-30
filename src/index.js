aa4a84a09f78c5805788a4113c6fc9f5c83fd8fc
andra manday <andra.manday@gmail.com>
2021-07-30 15:50:31 +0700

    add scc sass postcss hotreloading

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+import './styles/index.scss';
+
 const elevenShildRecipe = {
-    leatherScripts : 1,
-    ironIngot : 1,
-    refinedMoonstone : 4,
-    marko : 5
+    leatherScripts: 1,
+    ironIngot: 1,
+    refinedMoonstone: 4,
+    marko: 5
 }
 
 const neseRecipe = {
-    ... elevenShildRecipe,
-    kompor : 2,
-    sendok : 3
+    ...elevenShildRecipe,
+    kompor: 2,
+    sendok: 3
+}
+
+const test = {
+    nama: "budi"
 }
 
 console.log(elevenShildRecipe);
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,41 @@
-// let mode = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const target = process.env.NODE_ENV === "production" ? "browserslist" : "web";
+const devTool = process.env.NODE_ENV === "production" ? "source-map" : "eval";
 
-module.exports ={
-    // mode: mode,
-    devtool: false,
-    module : {
-        rules:[{
-            test : /\.js$/,
-            exclude : /node_modules/,
-            use: {
-                loader : "babel-loader"
+module.exports = {
+    target: target,
+    devtool: devTool,
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
             }
-        }]
+        ],
     },
-    devServer : {
-        contentBase: "./dist"    
+
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ],
+    devServer: {
+        contentBase: "./dist",
+        port: 3000,
+        hot: true
     }
 }
